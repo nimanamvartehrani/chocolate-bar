03576c93f4e9fd635848441a17abafcbc212694b
nimanamvartehrani <91355657+nimanamvartehrani@users.noreply.github.com>
2021-10-07 11:31:27 +0100

    Create chocolateCode.java

---
+++ chocolateCode.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+    
+        if (num3 < num1 * num2) {
+            if (num3 % num1 == 0 && num3 / num1 < num2 || num3 % num2 == 0 && num3 / num2 < num1) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO"); 
+            }    
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
